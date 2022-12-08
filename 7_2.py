8f1304dc081614e2429bda99eb7a1391b7ac4663
Insaf Latypov <saferif@gmail.com>
2022-12-08 01:17:37 +0000

    Day 7.

---
+++ 7_2.py
@@ -0,0 +1,42 @@
+import sys
+
+class Node(object):
+  def __init__(self, parent, name, isdir=False, size=0):
+    self.parent = parent
+    self.name = name
+    self.isdir = isdir
+    self.size = size
+    self.children = []
+
+  def update_size(self):
+    self.size = sum(x.size for x in self.children)
+    if self.parent:
+      self.parent.update_size()
+
+root = Node(None, '/', isdir=True)
+
+def dir_sizes(t):
+  yield t.size
+  for c in t.children:
+    if c.isdir:
+      yield from dir_sizes(c)
+
+cur = None
+
+for line in sys.stdin:
+  toks = line.split()
+  if toks[0] == '$':
+    if toks[1] == 'cd':
+      if toks[2] == '/':
+        cur = root
+      elif toks[2] == '..':
+        cur = cur.parent
+      else:
+        cur = next(filter(lambda x: x.isdir and x.name == toks[2], cur.children))
+  elif toks[0] == 'dir':
+    cur.children.append(Node(cur, toks[1], isdir=True))
+  else:
+    cur.children.append(Node(cur, toks[1], size=int(toks[0])))
+    cur.update_size()
+
+print(min(s for s in dir_sizes(root) if s >= 30000000 - (70000000 - root.size)))
