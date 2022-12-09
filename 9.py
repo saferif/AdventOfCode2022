a73c08d9625a699f2cae6c9f77844452270f9611
Insaf Latypov <saferif@gmail.com>
2022-12-09 14:54:22 +0000

    Day 9.

---
+++ 9.py
@@ -0,0 +1,30 @@
+import sys
+
+DIRS = {
+  'R': (1, 0),
+  'U': (0, -1),
+  'L': (-1, 0),
+  'D': (0, 1),
+}
+POS = set()
+KNOTS = 2
+ROPE = [(0, 0) for _ in range(KNOTS)]
+
+def sign(x):
+  if x < 0:
+    return -1
+  if x > 0:
+    return 1
+  return 0
+
+for line in sys.stdin:
+  toks = line.split()
+  dx, dy = DIRS[toks[0]]
+  for _ in range(int(toks[1])):
+    ROPE[0] = (ROPE[0][0] + dx, ROPE[0][1] + dy)
+    for i in range(1, len(ROPE)):
+      mx, my = ROPE[i-1][0] - ROPE[i][0], ROPE[i-1][1] - ROPE[i][1]
+      if abs(mx) > 1 or abs(my) > 1:
+        ROPE[i] = (ROPE[i][0] + sign(mx), ROPE[i][1] + sign(my))
+    POS.add(ROPE[-1])
+print(len(POS))
