faaee0e6051a78ee7dd8572f0793e8cecb210a37
Insaf Latypov <saferif@gmail.com>
2022-12-12 11:38:36 +0000

    Day 12.

---
+++ 12_2.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+
+DIRS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+MAP = [line.strip() for line in sys.stdin]
+STEPS = [[-1 for _ in range(len(MAP[i]))] for i in range(len(MAP))]
+
+fr, fc = 0, 0
+q = deque()
+for r in range(len(MAP)):
+  for c in range(len(MAP[r])):
+    if MAP[r][c] == 'S':
+      MAP[r] = MAP[r].replace('S', 'a')
+    if MAP[r][c] == 'a':
+      q.append((r, c, 0))
+    if MAP[r][c] == 'E':
+      fr, fc = r, c
+      MAP[r] = MAP[r].replace('E', 'z')
+
+while q:
+  cr, cc, step = q.popleft()
+  if STEPS[cr][cc] <= step and STEPS[cr][cc] != -1:
+    continue
+  STEPS[cr][cc] = step
+  for dr, dc in DIRS:
+    nr, nc = cr + dr, cc + dc
+    if 0 <= nr < len(MAP) and 0 <= nc < len(MAP[nr]) and ord(MAP[nr][nc]) - ord(MAP[cr][cc]) <= 1:
+      q.append((nr, nc, step + 1))
+
+print(STEPS[fr][fc])
